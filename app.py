87942b742536029aaaa3a5b7ebe2529af4ec25d5
MikAnd <micheli.andrea@gmail.com>
2015-09-30 16:44:00 +0200

    Added support for opencv1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import time
+
 from flask import Flask, render_template, Response
 from flask import request
 
@@ -19,6 +21,7 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.05)
 
 
 @app.route('/video_feed')
--- camera.py
+++ camera.py
@@ -1,11 +1,11 @@
-import cv2
+import cv
 
 class Camera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0)
+        self.video = cv.CaptureFromCAM(0)
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
@@ -14,17 +14,7 @@ class Camera(object):
         self.video.release()
 
     def get_frame(self):
-        success, image = self.video.read()
-        if success:
-            # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-            # so we must encode it into JPEG in order to correctly display the
-            # video stream.
-            small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-            ret, jpeg = cv2.imencode('.jpg', small)
-            if ret:
-                return jpeg.tostring()
-            else:
-                return None
-        else:
-            assert False
-            return None
+        image = cv.QueryFrame(self.video)
+        cv.SaveImage("/dev/shm/frame.jpg", image)
+        with open("/dev/shm/frame.jpg") as f:
+            return f.read()
