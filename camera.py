32bfd91f16f57dcf617277110b8847b4d1ac2b93
MikAnd <micheli.andrea@gmail.com>
2015-10-09 17:36:14 +0200

    Added hybrid support for CV2 and CV1 in master

--- camera.py
+++ camera.py
@@ -3,8 +3,13 @@
 import time
 import io
 import threading
-import cv2
 
+CV_VER = 2
+try:
+    import cv2
+except ImportError:
+    import cv
+    CV_VER = 1
 
 class Camera(object):
     thread = None  # background thread that reads frames from camera
@@ -28,23 +33,39 @@ class Camera(object):
 
     @classmethod
     def _thread(cls):
-        cam = cv2.VideoCapture(0)
-        while True:
-            if cam.isOpened():
-                success, image = cam.read()
-                if success:
-                    # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-                    # so we must encode it into JPEG in order to correctly display the
-                    # video stream.
-                    small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-                    ret, jpeg = cv2.imencode('.jpg', small)
-                    if ret:
-                        cls.frame = jpeg.tostring()
+        if CV_VER == 2:
+            cam = cv2.VideoCapture(0)
+            while True:
+                if cam.isOpened():
+                    success, image = cam.read()
+                    if success:
+                        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+                        # so we must encode it into JPEG in order to correctly display the
+                        # video stream.
+                        small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+                        ret, jpeg = cv2.imencode('.jpg', small)
+                        if ret:
+                            cls.frame = jpeg.tostring()
 
-                # if there hasn't been any clients asking for frames in
-                # the last 10 seconds stop the thread
-                if time.time() - cls.last_access > 10:
-                    break
-            time.sleep(1.0 / 30.0)
-        cam.release()
+                    # if there hasn't been any clients asking for frames in
+                    # the last 10 seconds stop the thread
+                    if time.time() - cls.last_access > 10:
+                        break
+                time.sleep(1.0 / 30.0)
+            cam.release()
+
+        else:
+            cam = cv.CaptureFromCAM(0)
+            while True:
+                image = cv.QueryFrame(cam)
+                cv.SaveImage("/dev/shm/frame.jpg", image)
+                with open("/dev/shm/frame.jpg") as f:
+                    cls.frame = f.read()
+
+                    # if there hasn't been any clients asking for frames in
+                    # the last 10 seconds stop the thread
+                    if time.time() - cls.last_access > 10:
+                        break
+                time.sleep(1.0 / 30.0)
+
         cls.thread = None
